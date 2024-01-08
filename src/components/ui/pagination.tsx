57e241fdea6f378e5dad3ae869cebd0622264555
Milind Mishra ▲ <milind.mishra4@gmail.com>
2024-01-08 22:32:42 +0530

    🐛 FIX: update pagination accent color

--- src/components/ui/pagination.tsx
+++ src/components/ui/pagination.tsx
@@ -28,7 +28,7 @@ export default function Pagination() {
         {/* Current: "border-indigo-500 text-indigo-600", Default: "border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300" */}
         <a
           href="#"
-          className="inline-flex items-center border-t-2 border-indigo-500 px-4 pt-4 text-sm font-medium text-indigo-600"
+          className="inline-flex items-center border-t-2 border-gray-500 px-4 pt-4 text-sm font-medium text-gray-600"
           aria-current="page"
         >
           2
