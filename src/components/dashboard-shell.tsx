afa3ed2117b0ae3db65622a51ab3ed8bf19bc85b
Milind Mishra ▲ <milind.mishra4@gmail.com>
2024-01-08 22:03:59 +0530

    🐛 FIX: update placeholder

--- src/components/dashboard-shell.tsx
+++ src/components/dashboard-shell.tsx
@@ -316,7 +316,7 @@ export default function DashboardShell({
             <div className="flex flex-1 gap-x-4 self-stretch lg:gap-x-6">
               <form className="relative flex flex-1" action="#" method="GET">
                 <label htmlFor="search-field" className="sr-only">
-                  Search
+                  Search features, tutorials, etc.
                 </label>
                 <MagnifyingGlassIcon
                   className="pointer-events-none absolute inset-y-0 left-0 h-full w-5 text-gray-400"
@@ -325,7 +325,7 @@ export default function DashboardShell({
                 <input
                   id="search-field"
                   className="block h-full w-full border-0 py-0 pl-8 pr-0 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm"
-                  placeholder="Search..."
+                  placeholder="Search features, tutorials, etc."
                   type="search"
                   name="search"
                 />
