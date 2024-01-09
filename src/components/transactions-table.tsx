f984f6def62c00eda3879df96132652928edd7bc
Milind Mishra ▲ <milind.mishra4@gmail.com>
2024-01-09 13:44:14 +0530

    🐛 FIX: add search input to transaction table

--- src/components/transactions-table.tsx
+++ src/components/transactions-table.tsx
@@ -5,7 +5,38 @@ import {
   InformationCircleIcon,
   MagnifyingGlassIcon,
 } from "@heroicons/react/20/solid";
+import {
+  BookmarkIcon,
+  ArrowsUpDownIcon,
+  ArrowDownTrayIcon,
+} from "@heroicons/react/20/solid";
 import { transactions } from "@/data/transactions";
+
+function TransactionButtonGroup() {
+  return (
+    <span className="isolate inline-flex rounded-md shadow-sm">
+      <button
+        type="button"
+        className="relative inline-flex items-center gap-x-1.5 rounded-l-md bg-white px-3 py-2 text-sm font-normal text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-10"
+      >
+        Sort
+        <ArrowsUpDownIcon
+          className="ml-0.5 h-5 w-5 text-gray-400"
+          aria-hidden="true"
+        />
+      </button>
+      <button
+        type="button"
+        className="relative -ml-px inline-flex items-center rounded-r-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-10"
+      >
+        <ArrowDownTrayIcon
+          className="ml-0.5 h-5 w-5 text-gray-400"
+          aria-hidden="true"
+        />
+      </button>
+    </span>
+  );
+}
 
 export default function TransactionsTable() {
   return (
@@ -21,11 +52,11 @@ export default function TransactionsTable() {
         <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
           <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
             <Card>
-              <div className="flex-1">
+              <div className="flex-1 flex items-center">
                 <label htmlFor="search" className="sr-only">
                   Search by Order ID
                 </label>
-                <div className="relative">
+                <div className="relative flex items-center">
                   <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
                     <MagnifyingGlassIcon
                       className="h-5 w-5 text-gray-400"
@@ -35,12 +66,16 @@ export default function TransactionsTable() {
                   <input
                     id="search"
                     name="search"
-                    className="block w-1/3 rounded-md border-0 bg-white py-1.5 pl-10 pr-3 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    className="block w-full rounded-md border-0 bg-white py-1.5 pl-10 pr-3 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-600 sm:text-sm sm:leading-6"
                     placeholder="Search by Order ID"
                     type="search"
                   />
                 </div>
+                <div className="ml-auto">
+                  <TransactionButtonGroup />
+                </div>
               </div>
+
               <table className="min-w-full divide-y divide-gray-300">
                 <thead>
                   <tr>
